f32e98149d23ea8e63c5a1a85f35aa8dacdd723c
Gina <gurubi.gina@gmail.com>
2021-03-17 14:14:51 +0100

    Quadratic solver initial version

---
+++ Quadratic Equation Solver/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masodfoku_egyenlet
+{
+    class Program
+    {
+        public struct QuadtraticEquation
+        {
+            public double A;
+            public double B;
+            public double C;
+        }
+
+        static void Main(string[] args)
+        {
+            QuadtraticEquation e = ReadEquation();
+
+            double[] r = Solve(e);
+
+            WriteResult(r);
+
+            Console.ReadLine();
+        }
+
+        private static void WriteResult(double[] r)
+        {
+            if (r.Length == 0)
+            {
+                Console.WriteLine("Nincs megoldas");
+            }
+
+            else if (r.Length == 1)
+            {
+                Console.WriteLine("A megoldas: " + r[0]);
+            }
+            else
+            {
+                Console.WriteLine("A megoldasok: " + r[0] + ", " + r[1]);
+            }
+        }
+
+        private static QuadtraticEquation ReadEquation()
+        {
+            QuadtraticEquation e = new QuadtraticEquation();
+
+            e.A = Convert.ToDouble(Console.ReadLine());
+            e.B = Convert.ToDouble(Console.ReadLine());
+            e.C = Convert.ToDouble(Console.ReadLine());
+            return e;
+        }
+
+        public static double Discriminant(QuadtraticEquation e)
+        {
+            return e.B * e.B - 4 * e.A * e.C;
+        }
+
+        public static double[] Solve(QuadtraticEquation e)
+        {
+            var d = Discriminant(e);
+            if (d < 0)
+            {
+                return new double[0];
+            }
+
+            var x1 = ((-e.B) + Math.Sqrt(d)) / (2 * e.A);
+            var x2 = ((-e.B) - Math.Sqrt(d)) / (2 * e.A);
+
+            if (x1 == x2)
+            {
+                return new double[] { x1 };
+            }
+
+            return new double[] { x1, x2 };
+        }
+    }
+}
