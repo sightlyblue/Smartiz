3df495b00893d59d4ea2910721dce7d1f833f85c
Gina Gurubi <gurubi.gina@gmail.com>
2021-04-02 19:44:55 +0200

    Refactoring, plus/minus button, change scaling (not working yet)

--- Quadratic Equation Plot/Program.cs
+++ Quadratic Equation Plot/Program.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 //TODO: input refactor
-//TODO: zérushelyek kirajzolása
+//TODO: zérushelyek kirajzolása  _/
 //TODO: tick-ek és rács rajzolása
-//TODO: skálázás állítása külön + és - gombokkal
+//TODO: skálázás állítása külön + és - gombokkal  _/
 
 //TODO: grafikus integrálszámítás
 //TODO: polinomok kirajzolása
-//TODO: pplinomok analízise: x0, min/max -> numerikusv v. diszkrét megoldás?
+//TODO: polinomok analízise: x0, min/max -> numerikusv v. diszkrét megoldás?
 
 //TODO: kör kirajzolása Circle hívás nélkül
 //TODO: kocka kirajzolása izometrikusan
@@ -20,40 +20,6 @@ using System.Windows.Forms;
 
 namespace Quadratic_Equation_Plot
 {
-    public struct Rectangle
-    {
-        public int Left;
-        public int Top;
-        public int Width;
-        public int Height;
-
-        public Rectangle(int left, int top, int width, int height)
-        {
-            Left = left;
-            Top = top;
-            Width = width;
-            Height = height;
-        }
-    }
-
-    public struct Button
-    {
-        public Rectangle Bounds;
-
-        public Direction Direction;
-
-        public Button(int left, int top, int width, int height, Direction direction)
-        {
-            Bounds = new Rectangle(left, top, width, height);
-            Direction = direction;
-        }
-    }
-
-    public enum Direction
-    {
-        Up,
-        Down
-    }
 
     public class Program : SmartizSketch
     {
@@ -66,7 +32,7 @@ namespace Quadratic_Equation_Plot
         double a = 0.1;
         double b = 10;
         double c = -8;
-        double scale = 1;
+        double scale = 5;
         int TposX = 20;
         int TposY = 40;
         int RposX;
@@ -93,8 +59,8 @@ namespace Quadratic_Equation_Plot
             Button4 = new Button(RposX, TposY + 25 + ButtonHeight, ButtonWidth, ButtonHeight, Direction.Down);
             Button5 = new Button(RposX, TposY + 65, ButtonWidth, ButtonHeight, Direction.Up);
             Button6 = new Button(RposX, TposY + 65 + ButtonHeight, ButtonWidth, ButtonHeight, Direction.Down);
-            ButtonPlus = new Button(Width - 60, TposY - 15, ButtonWidth + 10, ButtonHeight + 10, Direction.Up);
-            ButtonMinus = new Button(Width - 60, TposY - 15 + ButtonHeight + 10, ButtonWidth + 10, ButtonHeight + 10, Direction.Down);
+            ButtonPlus = new Button(Width - 60, TposY - 15, ButtonWidth + 10, ButtonHeight + 10, Direction.Up, Symbol.Sign);
+            ButtonMinus = new Button(Width - 60, TposY - 15 + ButtonHeight + 10, ButtonWidth + 10, ButtonHeight + 10, Direction.Down, Symbol.Sign);
         }
 
         public override void DrawFrame()
@@ -112,19 +78,23 @@ namespace Quadratic_Equation_Plot
             StrokeWeight(1);
             Line(Width / 2, 0, Width / 2, Height);
             Line(0, Height / 2, Width, Height / 2);
-            double stepX = Width / scale;
-            double stepY = Height / scale;
-            for (double i = 0; i < Width; i += 10)
+            double stepX = Width / (scale*10);
+            double stepY = Height / (scale*10);
+
+            for (double i = 0; i < Height; i += stepY)
             {
                 Stroke(113, 114, 120, 150);
-                Line(0, scale * i, Width, scale * i);
+                Line(0, i, Width, i);
                 Stroke(255);
-                Line(Width / 2 - 2, scale * i, Width / 2 + 2, scale * i);
+                Line(Width / 2 - 2, i, Width / 2 + 2, i);
+            }
 
+            for (double i = 0; i < Width; i += stepX)
+            {
                 Stroke(113, 114, 120, 150);
-                Line(scale * i, 0, scale * i, Height);
+                Line(i, 0, i, Height);
                 Stroke(255);
-                Line(scale * i, Height / 2 - 2, scale * i, Height / 2 + 2);
+                Line(i, Height / 2 - 2, i, Height / 2 + 2);
             }
         }
 
@@ -159,14 +129,25 @@ namespace Quadratic_Equation_Plot
             Stroke(255);
             Rect(b.Bounds.Left, b.Bounds.Top, b.Bounds.Width, b.Bounds.Height);
             Fill(255);
-            if (b.Direction == Direction.Up)
+            if (b.Symbol == Symbol.Arrow && b.Direction == Direction.Up)
             {
                 Triangle(b.Bounds.Left + s, b.Bounds.Top + b.Bounds.Height - s, b.Bounds.Left + b.Bounds.Width / 2, b.Bounds.Top + s, b.Bounds.Left + b.Bounds.Width - s, b.Bounds.Top + b.Bounds.Height - s);
             }
-            else
+            else if (b.Symbol == Symbol.Arrow && b.Direction == Direction.Down)
             {
                 Triangle(b.Bounds.Left + s, b.Bounds.Top + s, b.Bounds.Left + b.Bounds.Width - s, b.Bounds.Top + s, b.Bounds.Left + b.Bounds.Width / 2, b.Bounds.Top + b.Bounds.Height - s);
             }
+            else if (b.Symbol == Symbol.Sign && b.Direction == Direction.Up)
+            {
+                TextSize(30);
+                Text("+", b.Bounds.Left + s + 3, b.Bounds.Top + b.Bounds.Height - s + 3);
+            }
+            else
+            {
+                TextSize(30);
+                Text("–", b.Bounds.Left + s + 3, b.Bounds.Top + b.Bounds.Height - s + 3);
+            }
+
         }
 
         public bool IsOn(Button b)
@@ -222,7 +203,7 @@ namespace Quadratic_Equation_Plot
 
             for (var x = -Width / 2; x < Width; x++)
             {
-                var y = scale * a * x * x + scale * b * x + scale * c;
+                var y = a * Math.Pow(x, 2) + b * Math.Pow(x, 1) + c;
                 Stroke(80, 133, 188);
                 StrokeWeight(2);
                 double X = x + Width / 2;
@@ -235,7 +216,7 @@ namespace Quadratic_Equation_Plot
 
         public void DrawRoots(double a, double b, double c)
         {
-            var d = b * b - 4 * a * c;
+            var d = (b * b - 4 * a * c);
             var x1 = ((-b) + Math.Sqrt(d)) / (2 * a);
             var x2 = ((-b) - Math.Sqrt(d)) / (2 * a);
 
@@ -243,6 +224,7 @@ namespace Quadratic_Equation_Plot
             var px2 = x2 + Width / 2;
             Stroke(200, 0, 0);
             StrokeWeight(10);
+            TextSize(15);
             Text("x1", Round(px1 + 5), Height / 2 + 10);
             Point(px1, Height / 2);
             Text("x2", Round(px2 + 5), Height / 2 + 10);
---
+++ Quadratic Equation Plot/Symbol.cs
@@ -0,0 +1,21 @@
+﻿//TODO: input refactor
+//TODO: zérushelyek kirajzolása
+//TODO: tick-ek és rács rajzolása
+//TODO: skálázás állítása külön + és - gombokkal
+
+//TODO: grafikus integrálszámítás
+//TODO: polinomok kirajzolása
+//TODO: pplinomok analízise: x0, min/max -> numerikusv v. diszkrét megoldás?
+
+//TODO: kör kirajzolása Circle hívás nélkül
+//TODO: kocka kirajzolása izometrikusan
+//TODO: kocka forgatása Y tengely körül
+
+namespace Quadratic_Equation_Plot
+{
+    public enum Symbol
+    {
+        Arrow,
+        Sign
+    }
+}
